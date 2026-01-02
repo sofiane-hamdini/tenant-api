6e089bc5e0ec9ff6070245eb738f47525b23ae9a
Sofiane Hamdini <sofiane.hamdini@ulaval.ca>
2026-01-01 19:23:39 -0500

    feat : Added complete logging

--- TentantAPI/Program.cs
+++ TentantAPI/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,15 +7,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddHealthChecks();
+
+builder.Host.UseSerilog((context, services, configuration) =>
+{
+    configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services)
+    .Enrich.FromLogContext()
+    .Enrich.WithProperty("Application", "TenantAPI")
+    .WriteTo.Console()
+    .WriteTo.Seq("http://localhost:5341");
+
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
     app.MapOpenApi();
-}
 
+app.MapHealthChecks("/healthz");
+app.MapOpenApi("/swagger");
+app.UseSerilogRequestLogging(options =>
+{
+    options.MessageTemplate = "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
